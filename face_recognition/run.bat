78dbe7f4a6e8d208fa1196fcee92284963b80495
Samuel Navas <snavas@users.noreply.github.com>
2017-07-10 20:54:38 +0200

    Update run.bat

--- face_recognition/run.bat
+++ face_recognition/run.bat
@@ -1,3 +1,4 @@
 python retrain.py --bottleneck_dir=bottlenecks --how_many_training_steps=50 --model_dir=inception --summaries_dir=training_summaries/basic --output_graph=retrained_graph.pb --output_labels=retrained_labels.txt --image_dir=data
+pause
 python run_test.py
 pause
